f792ebb3b35e62126ed2ea1816fe81e6a68cc008
Ameliadaa <ameliadia722@gmail.com>
2024-09-13 17:36:03 +0700

    Update layout.js

--- Intern_Tonjoo_FE/uploader/src/app/layout.js
+++ Intern_Tonjoo_FE/uploader/src/app/layout.js
@@ -21,12 +21,15 @@ export default function Layout({ children }) {
       </head>
       <body className="bg-gray-100 text-gray-800">
         <nav className="bg-primary p-4">
-          <div className="container mx-auto">
-            <h1 className="text-white text-2xl">Tailwind CSS</h1>
+          <div className="container mx-auto flex flex-row justify-between">
+            <h1 className="text-white text-2xl">Reuploader</h1>
+            <button className="bg-blue-700 text-white px-6 py-2 rounded min-w-24 rounded-lg">
+              Login
+            </button>
           </div>
         </nav>
         <main className="container mx-auto mt-10">{children}</main>
       </body>
     </html>
-  )
+  );
 }
