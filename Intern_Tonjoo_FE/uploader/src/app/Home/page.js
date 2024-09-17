956a8937a6544f9824606faf67d48571729b5842
Ameliadaa <ameliadia722@gmail.com>
2024-09-17 17:07:21 +0700

    Update page.js

--- Intern_Tonjoo_FE/uploader/src/app/Home/page.js
+++ Intern_Tonjoo_FE/uploader/src/app/Home/page.js
@@ -43,21 +43,21 @@ export default function Home() {
             <ul className="flex border-b">
               <li className="mr-1">
                 <Link
-                  className="inline-block py-2 px-4 text-blue-500 font-semibold bg-primary hover:text-white"
+                  className="inline-block py-2 px-4 font-semibold bg-primary text-white"
                   href="/"
                 >
                   Input Text
                 </Link>
               </li>
               <li className="mr-1">
-              <Link className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 hover:bg-primary hover:text-white "
+              <Link className="inline-block py-2 px-4 text-blue-500  hover:bg-primary hover:text-white "
                   href="/Pharsed">  Pharsed Text
               </Link>
                
               </li>
               <li className="mr-1">
                 <a
-                  className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 hover:bg-primary hover:text-white "
+                  className="inline-block py-2 px-4 text-blue-500  hover:bg-primary hover:text-white "
                   href="#"
                 >
                   URL List
