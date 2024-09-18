db1da374ab5bd0e76a3532b4268e6f1276823273
RohmaRifqiPamungkas <rohmarifqi31@gmail.com>
2024-09-18 09:01:01 +0700

    Commit breeze install

--- Intern_Tonjoo_BE/routes/api.php
+++ Intern_Tonjoo_BE/routes/api.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/upload', [UploadController::class, 'upload'])->middleware('auth:sanctum');
-
+});
