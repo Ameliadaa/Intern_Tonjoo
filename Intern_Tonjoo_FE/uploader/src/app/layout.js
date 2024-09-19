2420cc786e4922a3c232f279619b138bae9e64d5
Ameliadaa <ameliadia722@gmail.com>
2024-09-19 17:49:23 +0700

    Update layout.js

--- Intern_Tonjoo_FE/uploader/src/app/layout.js
+++ Intern_Tonjoo_FE/uploader/src/app/layout.js
@@ -17,9 +17,9 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="bg-gray-100 text-gray-800 font-sans">
-        <nav className="bg-primary p-4">
+        <nav className="bg-primary py-4 px-2">
           <div className="container mx-auto flex flex-row justify-between">
-            <h1 className="text-white text-2xl">Reuploader</h1>
+            <h1 className="text-white text-2xl">Tonjoo</h1>
             <button className="bg-blue-700 text-white px-6 py-2 rounded min-w-24 rounded-lg">
               Login
             </button>
