260101f5e6febae239f7f5f2c755a7488720120a
Ameliadaa <ameliadia722@gmail.com>
2024-09-20 12:52:06 +0700

    update componen navigasi reupload

---
+++ Intern_Tonjoo_FE/uploader/src/component/NavigasiReupload.js
@@ -0,0 +1,39 @@
+"use client"; 
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const NavigasiReupload = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg mb-6">
+      <ul className="flex border-b">
+        <li className="mr-1">
+          <Link
+            className={`inline-block py-2 px-4  text-black ${pathname === '/' ? 'bg-primary font-semibold text-white ' : 'hover:bg-secondary'}`}
+            href="/"
+          >
+            Input Text
+          </Link>
+        </li>
+        <li className="mr-1">
+          <Link
+            className={`inline-block py-2 px-4 text-black ${pathname === '/Pharsed' ? 'bg-primary text-white font-semibold' : 'hover:bg-secondary hover:text-white'}`}
+            href="/Pharsed"
+          >
+            Pharsed Text
+          </Link>
+        </li>
+        <li className="mr-1">
+            <Link  className={`inline-block py-2 px-4 text-black ${pathname === '/ListURL' ? 'bg-primary text-white font-semibold' : 'hover:bg-secondary hover:text-white'}`}
+            href="/ListURL"> URL List
+            </Link>
+        
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavigasiReupload;
